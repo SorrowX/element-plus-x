60d228b5bee679f612feeb39063e8752f8f6c1b8
徐志伟 <670451148@qq.com>
2025-04-01 19:08:57 +0800

    feat: add formily component

---
+++ docs/examples/f-form/form.vue
@@ -0,0 +1,70 @@
+<template>
+  <Form
+    :form="form"
+    :label-col="6"
+    :wrapper-col="10"
+    @auto-submit="log"
+    @auto-submit-failed="log"
+  >
+    <SchemaField>
+      <SchemaStringField
+        name="input"
+        title="输入框"
+        x-decorator="FormItem"
+        x-component="Input"
+        :x-component-props="{
+          clearable: true,
+        }"
+        :required="true"
+      />
+      <SchemaStringField
+        name="select"
+        title="选择框"
+        x-decorator="FormItem"
+        x-component="Select"
+        :x-component-props="{
+          clearable: true,
+        }"
+        :enum="[
+          {
+            label: '选项1',
+            value: 1,
+          },
+          {
+            label: '选项2',
+            value: 2,
+          },
+        ]"
+        :required="true"
+      />
+    </SchemaField>
+    <FormButtonGroup align-form-item>
+      <Submit>提交</Submit>
+    </FormButtonGroup>
+  </Form>
+</template>
+
+<script>
+import { createForm } from '@formily/core'
+import { createSchemaField } from '@formily/vue'
+import { Formily } from 'element-plus-x'
+const { Form, FormButtonGroup, FormItem, Input, Select, Submit } = Formily
+
+const form = createForm()
+const fields = createSchemaField({ components: { Input, Select, FormItem } })
+
+export default {
+  components: { FormButtonGroup, Submit, Form, ...fields },
+  data() {
+    return {
+      form,
+    }
+  },
+
+  methods: {
+    log(value) {
+      console.log(value)
+    },
+  },
+}
+</script>
--- packages/formily/form/index.ts
+++ packages/formily/form/index.ts
@@ -1,10 +1,9 @@
-import { Component, VNode, defineComponent } from 'vue'
-import type { SetupContext } from 'vue'
+import { defineComponent } from 'vue'
 import { Form as FormType, IFormFeedback } from '@formily/core'
-import { FormProvider as _FormProvider, useForm, h } from '@formily/vue'
-
+import { FormProvider as _FormProvider, h, useForm } from '@formily/vue'
 import { PreviewText } from '../preview-text'
 import { FormLayout, FormLayoutProps } from '../form-layout'
+import type { Component, SetupContext, VNode } from 'vue'
 
 const FormProvider = _FormProvider as unknown as Component
 
@@ -16,7 +15,7 @@ export interface FormProps extends FormLayoutProps {
   onAutoSubmitFailed?: (feedbacks: IFormFeedback[]) => void
 }
 
-export const Form = defineComponent({
+export const Form = defineComponent<FormProps>({
   name: 'FForm',
   props: [
     'form',
@@ -25,7 +24,7 @@ export const Form = defineComponent({
     'onAutoSubmit',
     'onAutoSubmitFailed',
   ],
-  setup(props: FormProps, { attrs, slots }: SetupContext) {
+  setup(props, { attrs, slots }: SetupContext) {
     const top = useForm()
 
     return () => {
@@ -33,7 +32,7 @@ export const Form = defineComponent({
         form,
         component = 'form',
         onAutoSubmit = attrs.onAutoSubmit,
-        onAutoSubmitFailed = attrs.autoSubmitFailed,
+        onAutoSubmitFailed = attrs.onAutoSubmitFailed,
         previewTextPlaceholder = slots?.previewTextPlaceholder,
       } = props
 
@@ -45,23 +44,27 @@ export const Form = defineComponent({
           },
           {
             default: () => [
-              h(FormLayout, { attrs }, {
-                default: () => [
-                  h(
-                    component,
-                    {
-                      onSubmit: (e: Event) => {
-                        e?.stopPropagation?.()
-                        e?.preventDefault?.()
-                        form
-                          .submit(onAutoSubmit as (e: any) => void)
-                          .catch(onAutoSubmitFailed as (e: any) => void)
+              h(
+                FormLayout,
+                { ...attrs },
+                {
+                  default: () => [
+                    h(
+                      component,
+                      {
+                        onSubmit: (e: Event) => {
+                          e?.stopPropagation?.()
+                          e?.preventDefault?.()
+                          form
+                            .submit(onAutoSubmit as (e: any) => void)
+                            .catch(onAutoSubmitFailed as (e: any) => void)
+                        },
                       },
-                    },
-                    slots
-                  ),
-                ],
-              }),
+                      slots
+                    ),
+                  ],
+                }
+              ),
             ],
           }
         )
