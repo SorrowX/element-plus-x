37a34bc458bb4db4ef9595fcfe0c8ded9deb815e
徐志伟 <670451148@qq.com>
2025-04-02 20:04:14 +0800

    docs: switch

---
+++ docs/examples/f-switch/json-schema.vue
@@ -0,0 +1,48 @@
+<template>
+  <Form :form="form">
+    <SchemaField :schema="schema" />
+    <Submit @submit="onSubmit">提交</Submit>
+  </Form>
+</template>
+
+<script>
+import { createForm } from '@formily/core'
+import { createSchemaField } from '@formily/vue'
+import { Formily } from 'element-plus-x'
+const { Form, FormItem, Submit, Switch } = Formily
+
+const schema = {
+  type: 'object',
+  properties: {
+    switch: {
+      type: 'boolean',
+      title: '开关',
+      'x-decorator': 'FormItem',
+      'x-component': 'Switch',
+    },
+  },
+}
+
+const form = createForm()
+const { SchemaField } = createSchemaField({
+  components: {
+    FormItem,
+    Switch,
+  },
+})
+
+export default {
+  components: { Form, SchemaField, Submit },
+  data() {
+    return {
+      form,
+      schema,
+    }
+  },
+  methods: {
+    onSubmit(value) {
+      console.log(value)
+    },
+  },
+}
+</script>
---
+++ docs/examples/f-switch/markup-schema.vue
@@ -0,0 +1,42 @@
+<template>
+  <FormProvider :form="form">
+    <SchemaField>
+      <SchemaBooleanField
+        name="switch"
+        title="开关"
+        x-decorator="FormItem"
+        x-component="Switch"
+      />
+    </SchemaField>
+    <Submit @submit="log">提交</Submit>
+  </FormProvider>
+</template>
+
+<script>
+import { createForm } from '@formily/core'
+import { FormProvider, createSchemaField } from '@formily/vue'
+import { Formily } from 'element-plus-x'
+const { FormItem, Submit, Switch } = Formily
+
+const form = createForm()
+const fields = createSchemaField({
+  components: {
+    FormItem,
+    Switch,
+  },
+})
+
+export default {
+  components: { FormProvider, ...fields, Submit },
+  data() {
+    return {
+      form,
+    }
+  },
+  methods: {
+    log(value) {
+      console.log(value)
+    },
+  },
+}
+</script>
