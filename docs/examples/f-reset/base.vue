1d10ac92f3cc7eb5c713b8d8bcf3cfd94801d494
Summer <1356988206@qq.com>
2025-04-06 18:12:48 +0800

    feat: 优化reset

---
+++ docs/examples/f-reset/base.vue
@@ -0,0 +1,53 @@
+<template>
+  <FormProvider :form="form">
+    <SchemaField>
+      <SchemaStringField
+        required
+        name="input1"
+        title="输入框1"
+        x-decorator="FormItem"
+        x-component="Input"
+      />
+      <SchemaStringField
+        required
+        title="输入框"
+        name="input2"
+        x-decorator="FormItem"
+        x-component="Input"
+        default="123"
+      />
+    </SchemaField>
+    <FormButtonGroup align-form-item>
+      <Reset>重置</Reset>
+    </FormButtonGroup>
+  </FormProvider>
+</template>
+
+<script>
+import { createForm } from '@formily/core'
+import { FormProvider, createSchemaField } from '@formily/vue'
+import { Formily } from 'element-plus-x'
+const { Reset, FormButtonGroup, FormItem, Input } = Formily
+
+const fields = createSchemaField({
+  components: {
+    FormItem,
+    Input,
+  },
+})
+
+export default {
+  components: {
+    FormProvider,
+    Reset,
+    FormButtonGroup,
+    ...fields,
+  },
+  data() {
+    const form = createForm()
+    return {
+      form,
+    }
+  },
+}
+</script>
--- packages/formily/reset/index.ts
+++ packages/formily/reset/index.ts
@@ -1,16 +1,16 @@
+import { defineComponent, h } from 'vue'
 import { IFieldResetOptions } from '@formily/core'
-import { h, useParentForm } from '@formily/vue'
 import { observer } from '@formily/reactive-vue'
-import { defineComponent } from 'vue'
-
-import type { ElButton as ElButtonProps } from 'element-plus'
+import { useParentForm } from '@formily/vue'
 import { ElButton } from 'element-plus'
+import type { ButtonProps as ElButtonProps } from 'element-plus'
 
-export type ResetProps = IFieldResetOptions & typeof ElButtonProps
+export type ResetProps = IFieldResetOptions & ElButtonProps
 
 export const Reset = observer(
-  defineComponent({
+  defineComponent<ResetProps>({
     name: 'FReset',
+    inheritAttrs: false,
     props: {
       forceClear: {
         type: Boolean,
@@ -21,7 +21,7 @@ export const Reset = observer(
         default: false,
       },
     },
-    setup(props, { attrs, slots }: any) {
+    setup(props, { attrs, slots }) {
       const formRef = useParentForm()
       return () => {
         const form = formRef?.value
@@ -29,17 +29,17 @@ export const Reset = observer(
           ElButton,
           {
             ...attrs,
-            onClick: (e: MouseEvent) => {
-              if (attrs?.click) {
-                if (attrs.click(e) === false) return
+            onClick: (e: any) => {
+              if (attrs?.onClick) {
+                if ((attrs as any).onClick(e) === false) return
               }
               form
                 ?.reset('*', {
                   forceClear: props.forceClear,
                   validate: props.validate,
                 })
-                .then(attrs.resetValidateSuccess as (e: any) => void)
-                .catch(attrs.resetValidateFailed as (e: any) => void)
+                .then(attrs.onResetValidateSuccess as (e: any) => void)
+                .catch(attrs.onResetValidateFailed as (e: any) => void)
             },
           },
           slots
