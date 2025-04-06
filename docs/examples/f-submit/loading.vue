04322dad727a07c496a415ec68fd18ccf1861109
Summer <1356988206@qq.com>
2025-04-06 17:32:36 +0800

    feat: 优化submit

---
+++ docs/examples/f-submit/loading.vue
@@ -0,0 +1,57 @@
+<template>
+  <FormProvider :form="form">
+    <SchemaField>
+      <SchemaStringField
+        required
+        name="input1"
+        title="输入框"
+        x-decorator="FormItem"
+        x-component="Input"
+      />
+      <SchemaStringField
+        required
+        title="输入框"
+        name="input2"
+        x-decorator="FormItem"
+        x-component="Input"
+      />
+    </SchemaField>
+    <FormButtonGroup align-form-item>
+      <Submit @submit="handleSubmit">提交</Submit>
+    </FormButtonGroup>
+  </FormProvider>
+</template>
+
+<script>
+import { createForm } from '@formily/core'
+import { FormProvider, createSchemaField } from '@formily/vue'
+import { Formily } from 'element-plus-x'
+const { Submit, FormButtonGroup, FormItem, Input } = Formily
+
+const fields = createSchemaField({ components: { FormItem, Input } })
+
+export default {
+  components: {
+    FormProvider,
+    Submit,
+    FormButtonGroup,
+    ...fields,
+  },
+  data() {
+    const form = createForm()
+    return {
+      form,
+    }
+  },
+  methods: {
+    handleSubmit(values) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          console.log(values)
+          resolve()
+        }, 2000)
+      })
+    },
+  },
+}
+</script>
--- packages/formily/submit/index.ts
+++ packages/formily/submit/index.ts
@@ -1,33 +1,26 @@
-// @ts-nocheck
-import { SetupContext, defineComponent } from 'vue'
-import { h, useParentForm } from '@formily/vue'
+import { defineComponent, h } from 'vue'
 import { IFormFeedback } from '@formily/core'
 import { observer } from '@formily/reactive-vue'
-
+import { useParentForm } from '@formily/vue'
 import { ElButton } from 'element-plus'
-import type { ElButton as ElButtonProps } from 'element-plus'
+import type { ButtonProps as ElButtonProps } from 'element-plus'
 
-export type ISubmitProps = {
+export interface ISubmitProps extends ElButtonProps {
   onClick?: (e: MouseEvent) => any
   onSubmit?: (values: any) => any
   onSubmitSuccess?: (payload: any) => void
   onSubmitFailed?: (feedbacks: IFormFeedback[]) => void
-} & typeof ElButtonProps
+}
 
 export const Submit = observer(
-  defineComponent({
+  defineComponent<ISubmitProps>({
     name: 'FSubmit',
     props: ['onClick', 'onSubmit', 'onSubmitSuccess', 'onSubmitFailed'],
-    setup(props, { attrs, slots }: SetupContext) {
+    setup(props, { attrs, slots }) {
       const formRef = useParentForm()
 
       return () => {
-        const {
-          onClick = attrs?.onClick,
-          onSubmit = attrs?.onSubmit,
-          onSubmitSuccess = attrs?.onSubmitSuccess,
-          onSubmitFailed = attrs?.onSubmitFailed,
-        } = props
+        const { onClick, onSubmit, onSubmitSuccess, onSubmitFailed } = props
 
         const form = formRef?.value
         return h(
@@ -36,8 +29,9 @@ export const Submit = observer(
             nativeType: attrs?.submit ? 'button' : 'submit',
             type: 'primary',
             ...attrs,
-            loading:
-              attrs.loading !== undefined ? attrs.loading : form?.submitting,
+            loading: (attrs.loading !== undefined
+              ? attrs.loading
+              : form?.submitting) as boolean,
             onClick: (e: any) => {
               if (onClick) {
                 if (onClick(e) === false) return
