4bb4f464a0944dcf4a57b39c74718d68179966e2
徐志伟 <670451148@qq.com>
2025-05-20 09:19:41 +0800

    feat: add video-player icon

--- docs/examples/icons-vue/basic.vue
+++ docs/examples/icons-vue/basic.vue
@@ -2,10 +2,11 @@
   <el-space :size="20">
     <el-icon :size="30" color="#ea1d26"><QQ /></el-icon>
     <el-icon :size="30" color="#00dc6c"><Wechat /></el-icon>
+    <el-icon :size="30"><VideoPlayer /></el-icon>
   </el-space>
 </template>
 
 <script lang="ts" setup>
 import { IconsVue } from 'element-plus-x'
-const { QQ, Wechat } = IconsVue
+const { QQ, Wechat, VideoPlayer } = IconsVue
 </script>
