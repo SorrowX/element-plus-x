56f13e2b6b0c6ec3df77823f0feffc4db1e8d754
徐志伟 <670451148@qq.com>
2025-05-27 11:43:29 +0800

    fix: json-schema demo

--- docs/examples/f-json-schema/json1.vue
+++ docs/examples/f-json-schema/json1.vue
@@ -154,52 +154,52 @@ const formProps = ref({
 const schema = {
   type: 'object',
   properties: {
-    // input: {
-    //   required: true,
-    //   type: 'string',
-    //   title: '输入框',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Input',
-    //   'x-component-props': {},
-    //   default: 'Summer',
-    // },
-    // mention: {
-    //   required: true,
-    //   type: 'string',
-    //   title: '提及',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Mention',
-    //   'x-component-props': {},
-    //   default: '@',
-    //   enum: options.value,
-    // },
-    // inputNumber: {
-    //   required: true,
-    //   type: 'number',
-    //   title: '数字',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'InputNumber',
-    //   'x-component-props': {},
-    //   default: 10,
-    // },
-    // select: {
-    //   required: true,
-    //   type: 'array',
-    //   title: '选择框',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Select',
-    //   'x-component-props': {
-    //     multiple: false,
-    //     clearable: true,
-    //     displayType: 'text',
-    //     separator: '、',
-    //     onChange() {
-    //       console.log('select change')
-    //     },
-    //   },
-    //   enum: ['选项一', '选项二'],
-    //   default: ['选项一', '选项二', 'sfsfd'],
-    // },
+    input: {
+      required: true,
+      type: 'string',
+      title: '输入框',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {},
+      default: 'Summer',
+    },
+    mention: {
+      required: true,
+      type: 'string',
+      title: '提及',
+      'x-decorator': 'FormItem',
+      'x-component': 'Mention',
+      'x-component-props': {},
+      default: '@',
+      enum: options.value,
+    },
+    inputNumber: {
+      required: true,
+      type: 'number',
+      title: '数字',
+      'x-decorator': 'FormItem',
+      'x-component': 'InputNumber',
+      'x-component-props': {},
+      default: 10,
+    },
+    select: {
+      required: true,
+      type: 'array',
+      title: '选择框',
+      'x-decorator': 'FormItem',
+      'x-component': 'Select',
+      'x-component-props': {
+        multiple: false,
+        clearable: true,
+        displayType: 'text',
+        separator: '、',
+        onChange() {
+          console.log('select change')
+        },
+      },
+      enum: ['选项一', '选项二'],
+      default: ['选项一', '选项二', 'sfsfd'],
+    },
     select2: {
       type: 'array',
       title: '虚拟选择器',
@@ -220,32 +220,32 @@ const schema = {
       },
       default: [1],
     },
-    // tree: {
-    //   required: true,
-    //   type: 'string',
-    //   title: '树形选择',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'TreeSelect',
-    //   'x-component-props': {
-    //     clearable: true,
-    //     displayType: 'text',
-    //   },
-    //   default: '1-1-1',
-    // },
-    // cascader: {
-    //   required: true,
-    //   type: 'string',
-    //   title: '级联选择',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Cascader',
-    //   'x-component-props': {
-    //     clearable: true,
-    //     style: {
-    //       width: '100%',
-    //     },
-    //   },
-    //   default: '1-1-1',
-    // },
+    tree: {
+      required: true,
+      type: 'string',
+      title: '树形选择',
+      'x-decorator': 'FormItem',
+      'x-component': 'TreeSelect',
+      'x-component-props': {
+        clearable: true,
+        displayType: 'text',
+      },
+      default: '1-1-1',
+    },
+    cascader: {
+      required: true,
+      type: 'string',
+      title: '级联选择',
+      'x-decorator': 'FormItem',
+      'x-component': 'Cascader',
+      'x-component-props': {
+        clearable: true,
+        style: {
+          width: '100%',
+        },
+      },
+      default: '1-1-1',
+    },
     datePicker: {
       required: true,
       type: 'string',
@@ -263,138 +263,138 @@ const schema = {
       },
       default: '2024/07/23',
     },
-    // timePicker: {
-    //   required: true,
-    //   type: 'string',
-    //   title: '时间',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'TimePicker',
-    //   'x-component-props': {
-    //     style: {
-    //       width: '100%',
-    //     },
-    //   },
-    //   default: '2024/07/23 15:27:10',
-    // },
-    // radio: {
-    //   required: true,
-    //   type: 'string',
-    //   title: '单选框',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Radio.Group',
-    //   'x-component-props': {
-    //     displayType: 'text',
-    //     separator: '、',
-    //   },
-    //   enum: [
-    //     {
-    //       label: '选项1',
-    //       value: 1,
-    //     },
-    //     {
-    //       label: '选项2',
-    //       value: 2,
-    //     },
-    //   ],
-    //   default: 1,
-    // },
-    // radio2: {
-    //   required: true,
-    //   type: 'string',
-    //   title: '单选框2',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Radio.Group',
-    //   'x-component-props': {
-    //     options: ['选项1', '选项2'],
-    //   },
-    //   default: '选项2',
-    // },
-    // checkbox: {
-    //   required: true,
-    //   type: 'array',
-    //   title: '复选框',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Checkbox.Group',
-    //   'x-component-props': {
-    //     displayType: 'text',
-    //   },
-    //   enum: [
-    //     {
-    //       label: '选项1',
-    //       value: 1,
-    //     },
-    //     {
-    //       label: '选项2',
-    //       value: 2,
-    //     },
-    //   ],
-    //   default: [1, 2],
-    // },
-    // checkbox2: {
-    //   required: true,
-    //   type: 'array',
-    //   title: '复选框2',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Checkbox.Group',
-    //   'x-component-props': {
-    //     options: ['选项1', '选项2'],
-    //   },
-    //   default: ['选项1'],
-    // },
-    // switch: {
-    //   type: 'boolean',
-    //   title: '开关',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Switch',
-    //   'x-component-props': {
-    //     activeValue: 1,
-    //     inactiveValue: 0,
-    //     activeText: '启用',
-    //     inactiveText: '禁用',
-    //   },
-    //   default: 0,
-    // },
-    // textArea: {
-    //   required: true,
-    //   type: 'boolean',
-    //   title: '文本框',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Input.TextArea',
-    //   'x-component-props': {},
-    //   default: 'hi 你好啊',
-    // },
-    // slider: {
-    //   type: 'number',
-    //   title: '滑块',
-    //   required: true,
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Slider',
-    //   'x-component-props': {
-    //     style: {
-    //       margin: '0 4px',
-    //     },
-    //     showStops: true,
-    //     step: 10,
-    //   },
-    //   default: 10,
-    // },
-    // actions: {
-    //   type: 'void',
-    //   title: ' ',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Actions',
-    //   'x-decorator-props': {
-    //     colon: false,
-    //   },
-    //   'x-component-props': {
-    //     submitText: '提交',
-    //     resetText: '重置',
-    //     resetProps: {
-    //       forceClear: true,
-    //     },
-    //     onSubmit,
-    //   },
-    // },
+    timePicker: {
+      required: true,
+      type: 'string',
+      title: '时间',
+      'x-decorator': 'FormItem',
+      'x-component': 'TimePicker',
+      'x-component-props': {
+        style: {
+          width: '100%',
+        },
+      },
+      default: '2024/07/23 15:27:10',
+    },
+    radio: {
+      required: true,
+      type: 'string',
+      title: '单选框',
+      'x-decorator': 'FormItem',
+      'x-component': 'Radio.Group',
+      'x-component-props': {
+        displayType: 'text',
+        separator: '、',
+      },
+      enum: [
+        {
+          label: '选项1',
+          value: 1,
+        },
+        {
+          label: '选项2',
+          value: 2,
+        },
+      ],
+      default: 1,
+    },
+    radio2: {
+      required: true,
+      type: 'string',
+      title: '单选框2',
+      'x-decorator': 'FormItem',
+      'x-component': 'Radio.Group',
+      'x-component-props': {
+        options: ['选项1', '选项2'],
+      },
+      default: '选项2',
+    },
+    checkbox: {
+      required: true,
+      type: 'array',
+      title: '复选框',
+      'x-decorator': 'FormItem',
+      'x-component': 'Checkbox.Group',
+      'x-component-props': {
+        displayType: 'text',
+      },
+      enum: [
+        {
+          label: '选项1',
+          value: 1,
+        },
+        {
+          label: '选项2',
+          value: 2,
+        },
+      ],
+      default: [1, 2],
+    },
+    checkbox2: {
+      required: true,
+      type: 'array',
+      title: '复选框2',
+      'x-decorator': 'FormItem',
+      'x-component': 'Checkbox.Group',
+      'x-component-props': {
+        options: ['选项1', '选项2'],
+      },
+      default: ['选项1'],
+    },
+    switch: {
+      type: 'boolean',
+      title: '开关',
+      'x-decorator': 'FormItem',
+      'x-component': 'Switch',
+      'x-component-props': {
+        activeValue: 1,
+        inactiveValue: 0,
+        activeText: '启用',
+        inactiveText: '禁用',
+      },
+      default: 0,
+    },
+    textArea: {
+      required: true,
+      type: 'boolean',
+      title: '文本框',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input.TextArea',
+      'x-component-props': {},
+      default: 'hi 你好啊',
+    },
+    slider: {
+      type: 'number',
+      title: '滑块',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Slider',
+      'x-component-props': {
+        style: {
+          margin: '0 4px',
+        },
+        showStops: true,
+        step: 10,
+      },
+      default: 10,
+    },
+    actions: {
+      type: 'void',
+      title: ' ',
+      'x-decorator': 'FormItem',
+      'x-component': 'Actions',
+      'x-decorator-props': {
+        colon: false,
+      },
+      'x-component-props': {
+        submitText: '提交',
+        resetText: '重置',
+        resetProps: {
+          forceClear: true,
+        },
+        onSubmit,
+      },
+    },
   },
 }
 </script>
