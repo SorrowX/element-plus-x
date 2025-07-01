640bc872dddf40d1c193c3cb6957f6284fbcc087
Summer <1356988206@qq.com>
2025-07-01 16:25:12 +0800

    feat: bubble markdown

--- docs/examples/bubble/markdown.vue
+++ docs/examples/bubble/markdown.vue
@@ -1,6 +1,7 @@
 <template>
   <el-bubble
     :key="count"
+    class="bubble-box"
     :content="content"
     :message-render="renderMarkdown"
     :typing="{ step: 5, interval: 60 }"
@@ -21,7 +22,9 @@ const content = ref(
 
   Link: [ElementPlus X](https://sorrowx.github.io/epx/zh-CN/component/overview.html)
 
+  ![可爱图片](https://realbot-oss.oss-accelerate.aliyuncs.com/scrm/2025/2025-05-20/16910ba6-0708-48d2-9693-27ea40e2cd30/1.jpg)
 
+  <video src="https://realbot-oss.oss-accelerate.aliyuncs.com/scrm/2025/2025-05-21/659fa31d-dc87-4882-8be5-1fba8fb7e6ff/flower.mp4" controls width="320"></video>
 `.trim()
 )
 
@@ -29,3 +32,11 @@ const md = markdownit({ html: true, breaks: true })
 
 const renderMarkdown = (content) => h('div', { innerHTML: md.render(content) })
 </script>
+
+<style lang="scss">
+.bubble-box {
+  img {
+    max-width: 200px;
+  }
+}
+</style>
